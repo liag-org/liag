5cfe11f176aba8b5b494d280f0a4b0001bc4d900
David Venin <david.venin@gmail.com>
2023-03-29 17:02:40 +0200

    netlify conf

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,22 @@
+const baseConfig =
+  process.env.NODE_ENV === "production"
+    ? // when running the Netify CLI or building on Netlify, we want to use
+      {
+        server: "./server.js",
+        serverBuildPath: ".netlify/functions-internal/server.js",
+      }
+    : // otherwise support running remix dev, i.e. no custom server
+      undefined;
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  ...baseConfig,
   future: {
     v2_routeConvention: true,
     unstable_tailwind: true,
   },
+  ignoredRouteFiles: ["**/.*"],
+  // add your own custom config here if you want to.
+  //
+  // See https://remix.run/docs/en/v1/file-conventions/remix-config
 };
